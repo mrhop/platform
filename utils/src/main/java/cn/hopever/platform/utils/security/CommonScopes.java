b3606777f938368242c09be0c3204b08f0240bc2
mrhop <233402923@qq.com>
2016-09-09 17:16:07 +0800

    init user framework

--- utils/src/main/java/cn/hopever/platform/utils/security/CommonScopes.java
+++ utils/src/main/java/cn/hopever/platform/utils/security/CommonScopes.java
@@ -3,6 +3,6 @@ package cn.hopever.platform.utils.security;
 /**
  * Created by Donghui Huo on 2016/9/9.
  */
-//include internal_client and outter_client
+//include internal_client and outer_client
 public class CommonScopes {
 }
