7c2a8bf7f8658ca63860d6a3b1beb43e8bafd78d
mrhop <233402923@qq.com>
2016-12-12 11:06:30 +0800

    user module finished

---
+++ utilsWeb/src/main/java/cn/hopever/platform/cms/UtilsWebApplication.java
@@ -0,0 +1,22 @@
+package cn.hopever.platform.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by Donghui Huo on 2016/8/19.
+ */
+@SpringBootApplication
+public class UtilsWebApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(UtilsWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UtilsWebApplication.class);
+    }
+}
